dccc08726e3c56befb7b4b2c8fd2dddd8d91088d
Sandy Pratama <sandy-pratama@jsgi.co.id>
2024-09-11 08:03:02 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Security;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Path to the executable file
+        string exePath = "C:\\Program Files (x86)\\iVMS-4200 Site\\iVMS-4200 Client\\Client\\iVMS-4200.Framework.C.exe";
+
+        // Username and password for admin account
+        string username = "username";
+        string password = "password";
+
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = exePath,
+            UseShellExecute = false,
+            Domain = "domain",
+            UserName = username
+        };
+
+        // Create a SecureString object for the password
+        SecureString securePassword = new SecureString();
+        foreach (char c in password)
+        {
+            securePassword.AppendChar(c);
+        }
+        startInfo.Password = securePassword;
+
+        // Set the Verb property to run the process as administrator
+        startInfo.Verb = "runas";
+
+        try
+        {
+            Process.Start(startInfo);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
